d37d97da5d67445f27c2152be0a3e0a121cb0406
KID <50666207+KID421@users.noreply.github.com>
2023-08-10 13:13:06 +0800

    Update 完成控制系統：移動與跳躍

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -9,30 +9,86 @@ namespace KID.TwoD
     {
         [SerializeField, Header("移動速度"), Range(0, 50)]
         private float moveSpeed = 3.5f;
+        [SerializeField, Header("檢查地板尺寸")]
+        private Vector3 v3CheckGroundSize = Vector3.one;
+        [SerializeField, Header("檢查地板位移")]
+        private Vector3 v3CheckGroundOffset = Vector3.zero;
+        [SerializeField, Header("要偵測地板的圖層")]
+        private LayerMask layerCheckGround;
+        [SerializeField, Header("跳躍力道"), Range(0, 3000)]
+        private float jumpPower = 1000;
 
         private Rigidbody2D rig;
+        private Animator ani;
+        private string parRun = "開關跑步";
+        private string parJump = "開關跳躍";
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(1, 0, 0.3f, 0.5f);
+            Gizmos.DrawCube(transform.position + v3CheckGroundOffset, v3CheckGroundSize);
+        }
 
         private void Awake()
         {
-            //print("<color=yellow>喚醒事件</color>");
-
             rig = GetComponent<Rigidbody2D>();
+            ani = GetComponent<Animator>();
         }
 
         private void Start()
         {
-            //print("<color=green>開始事件</color>");
+
         }
 
         private void Update()
         {
-            //print("<color=red>更新事件</color>");
+            MoveAndFlip();
+            Jump();
+        }
 
-            //float v = Input.GetAxis("Vertical");
-            //print("<color=#f66>垂直的值：" + v + "</color>");
-
+        /// <summary>
+        /// 移動與翻面
+        /// </summary>
+        private void MoveAndFlip()
+        {
             float h = Input.GetAxis("Horizontal");
             rig.velocity = new Vector2(h * moveSpeed, rig.velocity.y);
+
+            // if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            if (h < 0)
+            {
+                transform.eulerAngles = Vector3.zero;
+            }
+            // else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            else if (h > 0)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+
+            ani.SetBool(parRun, h != 0);
+        }
+
+        /// <summary>
+        /// 判定是否在地面 並且 按空白建跳躍
+        /// </summary>
+        private void Jump()
+        {
+            if (CheckGround() && Input.GetKeyDown(KeyCode.Space))
+            {
+                rig.AddForce(new Vector2(0, jumpPower));
+            }
+        }
+
+        /// <summary>
+        /// 檢查角色是否在地板
+        /// </summary>
+        /// <returns>是否在地版上</returns>
+        private bool CheckGround()
+        {
+            Collider2D hit = Physics2D.OverlapBox(transform.position + v3CheckGroundOffset, v3CheckGroundSize, 0, layerCheckGround);
+            
+            ani.SetBool(parJump, !hit);
+            return hit;
         }
     }
 }
