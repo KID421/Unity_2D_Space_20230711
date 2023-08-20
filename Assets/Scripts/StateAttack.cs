0ef7f15303cd9d92275e663082dbd0e13dc747b9
KID <50666207+KID421@users.noreply.github.com>
2023-08-20 22:37:44 +0800

    Update 攻擊

--- Assets/Scripts/StateAttack.cs
+++ Assets/Scripts/StateAttack.cs
@@ -5,28 +5,42 @@ namespace KID.TwoD
 
     public class StateAttack : State
     {
-        [SerializeField, Header("攻擊間隔"), Range(0, 5)]
-        private float attackInterval = 3;
+        [SerializeField, Header("檢查是否擊中時間"), Range(0, 5)]
+        private float attackCheckHitTime = 2.5f;
+        [SerializeField, Header("攻擊結束時間"), Range(0, 5)]
+        private float attackFinishTime = 4;
+        [SerializeField, Header("追蹤狀態")]
+        private StateTrack stateTrack;
 
         private string parAttack = "觸發攻擊";
         private float timer;
 
         private void Start()
         {
-            timer = attackInterval;
+
         }
 
         public override State RunCurrentState()
         {
-            if (timer >= attackInterval)
+            if (timer == 0)
             {
-                timer = 0;
                 ani.SetTrigger(parAttack);
             }
             else
             {
-                timer += Time.deltaTime;
+                if (timer >= attackCheckHitTime && timer < attackCheckHitTime + Time.deltaTime)
+                {
+                    print("<color=#f69>檢查是否擊中</color>");
+                }
+                else if (timer >= attackFinishTime)
+                {
+                    print("<color=#f96>攻擊結束!</color>");
+                    timer = 0;
+                    return stateTrack;
+                }
             }
+
+            timer += Time.deltaTime;
 
             return this;
         }
